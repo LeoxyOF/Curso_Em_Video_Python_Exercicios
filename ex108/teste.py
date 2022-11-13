54f1584605457fe7401bad9356173a301ea0a1e0
Leonardo de Paula <115586408+LeoxyOF@users.noreply.github.com>
2022-11-13 16:03:21 -0400

    Delete ex108 directory

--- ex108/teste.py
+++
@@ -1,8 +0,0 @@
-from ex108.moeda import metade, dobro, dez, treze, moeda
-
-p = float(input('Digite o preço R$'))
-
-print(f'A metade desse valor é {moeda(metade(p))}')
-print(f'O Dobro desse valor é {moeda(dobro(p))}')
-print(f'Aumentando 10% temos {moeda(dez(p))}')
-print(f'Diminuindo 13% temos {moeda(treze(p))}')
